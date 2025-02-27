e6f3f4bc4935529279f9b7ab078e0ceb098de05a
lucasdeprit <lucasdeprit@gmail.com>
2025-02-27 15:57:19 +0100

    delete gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://lucasdeprit.github.io',
-  base: '/astro5-blog/',
-  output: 'server'
 });
