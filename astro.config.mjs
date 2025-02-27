99633456552f609f7452dbcfffb507e818bc0593
lucasdeprit <lucasdeprit@gmail.com>
2025-02-27 15:14:08 +0100

    update in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base: 'astro5-blog',
+  base: '/astro5-blog/',
   output: 'static'
 });
